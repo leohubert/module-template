2b9b03ac724b5da016a82ef49172635b895c8b2f
Hubert Léo <leohub@live.fr>
2018-05-07 10:37:31 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export default class Module {
+
+    constructor() {
+        console.log('Construtor');
+    }
+
+    init() {
+        console.log('MODULE INIT');
+    }
+
+    start() {
+        console.log('MODULE STARTED')
+    }
+}
